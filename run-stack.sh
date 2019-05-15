26396ff4939550ebf9cdb2f236c07d6ad69520d2
EC2 Default User <ec2-user@ip-172-31-43-249.us-east-2.compute.internal>
2019-05-15 19:20:20 +0000

    adding missing new line break symbol in run-stack.sh

--- run-stack.sh
+++ run-stack.sh
@@ -7,6 +7,6 @@ aws cloudformation $ACTION \
     --capabilities CAPABILITY_NAMED_IAM \
     --parameters \
     ParameterKey=DockerImage,ParameterValue=247554193015.dkr.ecr.us-east-2.amazonaws.com/example-webapp-builder:$(git rev-parse HEAD) \
-    ParameterKey=VPC,ParameterValue=vpc-dd1f06b5
+    ParameterKey=VPC,ParameterValue=vpc-dd1f06b5 \
     ParameterKey=Cluster,ParameterValue=default2 \
     ParameterKey=Listener,ParameterValue=arn:aws:elasticloadbalancing:us-east-2:247554193015:listener/app/production-website/0b12fd17ea748ae3/e29e3c6bce4896cf
