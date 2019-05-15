e1dbc827d55e50b13f82dcad1b6c3d956b713657
EC2 Default User <ec2-user@ip-172-31-43-249.us-east-2.compute.internal>
2019-05-15 19:03:37 +0000

    adding ecs deploy

---
+++ run-stack.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+aws cloudformation $ACTION \
+    --region us-east-1 \
+    --stack-name $STACK_NAME \
+    --template-body file://service.yaml \
+    --capabilities CAPABILITY_NAMED_IAM \
+    --parameters \
+    ParameterKey=DockerImage,ParameterValue=247554193015.dkr.ecr.us-east-2.amazonaws.com/example-webapp-builder:$(git rev-parse HEAD) \
+    ParameterKey=VPC,ParameterValue=vpc-dd1f06b5
+    ParameterKey=Cluster,ParameterValue=default2 \
+    ParameterKey=Listener,ParameterValue=arn:aws:elasticloadbalancing:us-east-2:247554193015:listener/app/production-website/0b12fd17ea748ae3/e29e3c6bce4896cf
